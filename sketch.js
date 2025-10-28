d81e97a76526e7334eec30f6d663cea8c58afb9b
wizardabhi77 <vemulapalliabhinav@gmail.com>
2025-10-28 11:40:00 +0530

    Add grid of divs to index page
    sketch.js creates divs and styles.css arranges them in a grid with flexbox.

--- sketch.js
+++ sketch.js
@@ -1,3 +1,16 @@
 const container = document.querySelector('.container');
 
-container.textContent = 'Hello world!';
+
+let gridSize = 16;
+
+function createSketchPad (size) {
+    for (i=0; i < size**2; i++) {
+        let card = document.createElement('div');
+        card.setAttribute('class','cards');
+        card.textContent = 'I AM CARD' + i;
+        container.appendChild(card);
+        
+    }
+}
+
+createSketchPad(gridSize);
