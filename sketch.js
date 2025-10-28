d378813d81af52cfd0d2a20e820f81dc38d53879
wizardabhi77 <vemulapalliabhinav@gmail.com>
2025-10-28 20:34:57 +0530

    Add button that set grid size
    Remove all cards when button clicked and create new grid with height and width from prompts

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', handleClick);
 
 
-let gridSize = 16;
-
-function createSketchPad (size) {
-    for (i=0; i < size**2; i++) {
+function createSketchPad (width,height) {
+    while(container.firstElementChild) {
+        container.removeChild(container.lastElementChild)
+    }
+    for (i=0; i < width*height; i++) {
         let card = document.createElement('div');
         card.setAttribute('class','cards');
         card.textContent = i+1;
@@ -15,8 +19,22 @@ function createSketchPad (size) {
 }
 
 function handleHover (e) {
-    e.target.style.backgroundColor = 'blue';
+    e.target.style.backgroundColor = 'pink';
 }
 
-createSketchPad(gridSize);
+function handleClick(e) {
+    let width = Number(prompt('Enter a valid width for grid:','1-100'));
+    if(width <= 0 || width > 100){
+        alert('Invalid width');
+        return null;
+    }
+    let height =  Number(prompt('Enter a valid height for grid:','1-100'));
+    if(height <= 0 || height > 100){
+        alert('Invalid height');
+        return null;
+    }
+
+    createSketchPad(width,height);
+
+}
 
